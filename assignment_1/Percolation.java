7a0fce73e2598ce784d0c327b530df0a8a182e86
Serj <seancher@jyu.fi>
2013-09-05 10:41:58 +0300

    Realizes the following concept. There are two sites. The  first one corresponds to open sites, the second to the filled sites
    Compilation:  PASSED
    Style:        PASSED
    Findbugs:     No potential bugs found.
    API:          PASSED
    
    Correctness:  20/20 tests passed
    Memory:       7/8 tests passed
    Timing:       9/9 tests passed
    
    Raw score: 98.75% [Correctness: 65%, Memory: 10%, Timing: 25%, Style: 0%]

--- assignment_1/Percolation.java
+++ assignment_1/Percolation.java
@@ -1,14 +1,19 @@
 public class Percolation {
-    private boolean[] openSites;
+    private boolean[] openSites, filledSites;
     private int size;    
-    private WeightedQuickUnionUF sites;
+    private WeightedQuickUnionUF opSites, fiSites;
     public Percolation(int N) {              // create N-by-N grid, with all sites blocked
-        sites = new WeightedQuickUnionUF(N * N + 1);
+        opSites = new WeightedQuickUnionUF(N * N + 2);
+        fiSites = new WeightedQuickUnionUF(N * N + 1);
         size = N;
-        openSites = new boolean[N * N + 1];
+        openSites = new boolean[N * N + 2];
+        filledSites = new boolean[N * N + 1];
         for (int i = 1; i <= N * N; i++) {
-            openSites[i] = false; }
-        openSites[0] = true; }
+            openSites[i] = false;
+            filledSites[i] = false; }
+        openSites[0] = true;
+        openSites[N * N + 1] = true;
+        filledSites[0] = true; }
    
     public void open(int i, int j) {        // open site (row i, column j) if it is not already
         if (i < 1 || j < 1 || i > size || j > size) {
@@ -16,27 +21,34 @@ public class Percolation {
         int p = size * (i - 1) + j; // get site id
         int q;
         openSites[p] = true;
+        filledSites[p] = true;
         // check up, down, left, right neighbors. if they are open then union sites
         q = size * (i - 2) + j; // check up
         if (i > 1) { // if it has up heighbor
             if (openSites[q]) {
-                sites.union(p, q); } }
-        else sites.union(p, 0);
+                opSites.union(p, q); 
+                fiSites.union(p, q); } }
+        else { opSites.union(p, 0);
+            fiSites.union(p, 0); }  
         
         q = size * i + j; // check down heighbor
         if (i < size) { // if it has down heighbor
             if (openSites[q]) {
-                sites.union(p, q); } }
+                opSites.union(p, q); 
+                fiSites.union(p, q); } }
+        else opSites.union(p, size * size + 1);
         
         q = size * (i - 1) + j - 1; // check left heighbor
         if (j > 1) { // if it has left heighbor
             if (openSites[q]) {
-                sites.union(p, q); } }
+                opSites.union(p, q);
+                fiSites.union(p, q); } }
         
         q = size * (i - 1) + j + 1; // check right heighbor
         if (j < size) { // if it has right heighbor
             if (openSites[q]) {
-                sites.union(p, q); } } }    
+                opSites.union(p, q);
+                fiSites.union(p, q); } } }    
    
     public boolean isOpen(int i, int j) {      // is site (row i, column j) open?
         if (i < 1 || j < 1 || i > size || j > size) {
@@ -46,13 +58,10 @@ public class Percolation {
     public boolean isFull(int i, int j) {      // is site (row i, column j) full?
         if (i < 1 || j < 1 || i > size || j > size) {
             throw new java.lang.IndexOutOfBoundsException(); }        
-        return sites.connected(0, size * (i - 1) + j); }
+        return fiSites.connected(0, size * (i - 1) + j); }
    
     public boolean percolates() {             // does the system percolate?
-        for (int j = 1; j <= size; j++) {
-            if (sites.connected(0, size * (size - 1) + j)) {
-                return true; } } 
-        return false; }
+        return opSites.connected(0, size * size + 1); } 
    
     public static void main(String[] args) {
         Percolation pSites = new Percolation(5);
